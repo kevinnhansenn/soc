9d5e899fad2c077dd09cc97a68afcb35ffb31fb1
Kevinhansen36 <kevinhansen36@yahoo.com>
2021-02-26 18:42:29 +0800

    another plan

---
+++ client/src/project/entrypoint.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Entrypoint() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Template</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Entrypoint
