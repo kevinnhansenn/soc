1f257075c34e8d44b6af7714c8e6d23f85db5f25
Kevinhansen36 <kevinhansen36@yahoo.com>
2021-03-09 11:56:46 +0800

    Server Draft

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import http from 'http'
 import { Server } from 'socket.io'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import _ from 'lodash'
 
 const app = express()
 
@@ -17,63 +18,120 @@ const options = {
     }
 }
 const io = new Server(httpServer, options)
+const studentIO = io.of('/student')
+const instructorIO = io.of('/instructor')
 
-io.of('/student').on('connection', (socket) => {
-    console.log(`Student connection has been established: ${socket.id}`)
+interface Choice {
+    id: string,
+    answer: string,
+    isAnswer: boolean
+}
 
-    /*
-        TIMELINE:
+interface Question {
+    id: string,
+    question: string,
+    choices: Choice[]
+}
 
-        [CLIENT - INSTRUCTOR] emit CREATE_ROOM
-        [SERVER - INSTRUCTOR] intercept CREATE_ROOM -> return status
+interface Room {
+    id: string,
+    host: string,
+    participants: string[]
+    question?: Question[]
+}
 
-        [CLIENT - STUDENT] emit JOIN_ROOM
-        [SERVER - STUDENT] intercept JOIN_ROOM -> Check room exist -> return status
+const genId = () => Math.floor(Math.random() * 100000).toString()
 
-        [SERVER - STUDENT] emit INFORM_INSTRUCTOR
-        [CLIENT - INSTRUCTOR] intercept STUDENT_JOIN_ROOM -> Update view
+const activeRooms: Room[] = []
 
-        List of events to intercept:
-            - JOIN_ROOM
-                - REQ: room_name
-                - RES: status
-             -
+studentIO.on('connection', (socket) => {
+    const { username, room } = socket.handshake.query
+    const roomFound = activeRooms.filter(activeRoom => activeRoom.id === room)
+    if (!roomFound.length) return socket.disconnect()
+    socket.join(room)
 
-         List of events to emit:
-             -
-     */
+    // Add student to the room
+    const _room:Room = roomFound[0]
+    _room.participants.push(username)
 
-    socket.on('JOIN_ROOM', (room) => {
-        socket.join(room)
+    socket.on('ANSWER_THE_QUESTION', (questionId, answerId) => {
+        const _question = _.find(_room.question, (question) => question.id === questionId)
+        const correctAnswer = _.find(_question.choices, (choice) => choice.id === answerId)
+
+        const response = {
+            room,
+            student: username,
+            questionId,
+            answerId,
+            result: 0
+        }
+
+        if (correctAnswer.isAnswer) {
+            response.result = 1
+        } else {
+            response.result = -1
+        }
+
+        studentIO.to(room).emit('UPDATE_SCOREBOARD', response)
+        instructorIO.to(room).emit('UPDATE_SCOREBOARD', response)
     })
 })
 
-io.of('/instructor').on('connection', (socket) => {
-    console.log(`Instructor connection has been established: ${socket.id}`)
+instructorIO.on('connection', (socket) => {
+    // CREATE_ROOM
+    const { username, room } = socket.handshake.query
+    socket.join(room)
 
-    /*
-        List of events to intercept:
-             - STUDENT_JOIN_ROOM
-                - RES: Student Name
+    const _room: Room = {
+        id: room,
+        host: username,
+        participants: [],
+        question: []
+    }
 
-         List of events to emit:
-             - CREATE_ROOM
-                - REQ: room_name
-                - RES: status
-             - PUBLISH_QUESTION
-                -
-     */
+    activeRooms.push(_room)
+
+    socket.on('START_SESSION', (callback) => {
+        studentIO.to(room).emit('SESSION_HAS_STARTED')
+        callback()
+    })
+
+    socket.on('POST_QUESTION', (question, callback) => {
+        _room.question.push(question)
+        studentIO.to(room).emit('QUESTION_HAS_BEEN_POSTED', question)
+        callback()
+    })
+
+    socket.on('FINISH_SESSION', (callback) => {
+        _.filter(activeRooms, activeRoom => activeRoom.id !== room)
+        studentIO.to(room).emit('SESSION_HAS_ENDED')
+        callback()
+    })
 })
 
 app.get('/', (req, res) => {
     res.send('GET REQUEST to backend server should not be allowed')
 })
 
+app.post('studentLogin', (req, res) => {
+    const { room } = req.body
+
+    const roomFound = activeRooms.filter(activeRoom => activeRoom.id === room)
+
+    if (!roomFound.length) return res.status(400).send('Invalid room number')
+
+    res.status(200).send({ room: roomFound })
+})
+
 app.post('/instructorLogin', (req, res) => {
-    const room = Math.floor(Math.random() * 100000)
+    const room = genId()
     const { username, password } = req.body
     if (!username || !password) return res.status(400).send('Invalid Credentials')
-
+    activeRooms.push({
+        id: room,
+        host: username,
+        participants: []
+    })
     res.status(200).send({ room })
 })
 
